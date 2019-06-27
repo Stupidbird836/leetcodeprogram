65841bc2c9efa1dfd81adc439521ac287d6d5fa9
huchenfei <huchenfei@chunbo.com>
2019-06-27 20:27:32 +0800

    回文字符串和 strStr

---
+++ src/leetcode/String/important/StrStr.java
@@ -0,0 +1,56 @@
+package leetcode.String.important;
+
+/**
+ * @Author：huchenfei
+ * @ClassName：StrStr
+ * @Description：28. 实现strStr()
+ * <p>
+ * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: haystack = "hello", needle = "ll"
+ * 输出: 2
+ * 示例 2:
+ * <p>
+ * 输入: haystack = "aaaaa", needle = "bba"
+ * 输出: -1
+ * 说明:
+ * <p>
+ * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ * <p>
+ * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+ * @Date：2019/6/27 19:29
+ * @Version： 1.0
+ **/
+public class StrStr {
+
+    public static void main(String... args) {
+        String haystack = "mississippi";
+        String needle = "pi";
+        System.out.println(strStr(haystack, needle));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        int length = haystack.length();
+        int length1 = needle.length();
+        if (length < length1) {
+            return -1;
+        }
+        if (length == length1 && haystack.equals(needle)) {
+            return 0;
+        }
+        for (int i = 0; i <= length - length1; i++) {
+            int j;
+            for (j = 0; j < length1; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+            }
+            if (j == length1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/leetcode/String/ordinary/IsPalindrome.java
@@ -0,0 +1,55 @@
+package leetcode.String.ordinary;
+
+/**
+ * @Author：huchenfei
+ * @ClassName：IsPalindrome
+ * @Description：给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。 <p><p/>
+ * 说明：本题中，我们将空字符串定义为有效的回文串。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "A man, a plan, a canal: Panama"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: "race a car"
+ * 输出: false
+ * @Date：2019/6/27 19:57
+ * @Version： 1.0
+ **/
+public class IsPalindrome {
+
+    public static void main(String... args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0){
+            return true;
+        }
+        s = s.toLowerCase();
+        for (int i = 0, j = s.length() - 1; i <= j; ) {
+            char p = s.charAt(i);
+            char q = s.charAt(j);
+            // p不为数字或字符时i往后找到第一个符合条件
+            if (!((p >= '0' && p <= '9') || (p >= 'a' && p <= 'z'))) {
+                i++;
+                continue;
+            }
+            // q不为数字或字符时i往前找到第一个符合条件
+            if (!((q >= '0' && q <= '9') || (q >= 'a' && q <= 'z'))) {
+                j--;
+                continue;
+            }
+            //当pq所指字符为数字和字母时才比较，如果字符或数字不相等立刻返回false
+            if (p != q) {
+                return false;
+            } else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
