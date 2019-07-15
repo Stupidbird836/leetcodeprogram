07ee484b2282d7ad064ec50b266402e0ca995730
huchenfei <huchenfei@chunbo.com>
2019-07-15 19:32:42 +0800

    最长公共前缀

---
+++ src/leetcode/String/ordinary/LongestCommonPrefix.java
@@ -0,0 +1,62 @@
+package leetcode.String.ordinary;
+
+import java.util.logging.Level;
+
+/**
+ * @Author：huchenfei
+ * @ClassName：LongestCommonPrefix
+ * @Description：最长公共前缀 编写一个函数来查找字符串数组中的最长公共前缀。
+ * <p>
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 示例 2:
+ * <p>
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 说明:
+ * <p>
+ * 所有输入只包含小写字母 a-z 。
+ * @Date：2019/7/15 19:08
+ * @Version： 1.0
+ **/
+public class LongestCommonPrefix {
+
+    public static void main(String... args) {
+        String[] strs = new String[]{"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    private static String longestCommonPrefix(String[] strs) {
+        if (strs ==null || strs.length <= 0) {
+            return "";
+        }
+        String str0 = strs[0];
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].length() < str0.length()) {
+                str0 = strs[i];
+            }
+        }
+        int length = str0.length();
+        while (length > 0) {
+            int temp = 0;
+            for (String str : strs) {
+                if (!str.startsWith(str0)) {
+                    length--;
+                    str0 = str.substring(0, length);
+                    break;
+                } else {
+                    temp++;
+                }
+            }
+            if (temp == strs.length) {
+                return str0;
+            }
+        }
+        return "";
+    }
+}
