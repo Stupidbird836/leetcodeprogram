3081afb99b0d8e56b9718b8c98dc95305decbafb
huchenfei <huchenfei@chunbo.com>
2019-12-25 20:18:22 +0800

    修改代码

--- src/leetcode/String/important/FirstUniqChar.java
+++ src/leetcode/String/important/FirstUniqChar.java
@@ -5,18 +5,15 @@ import java.util.HashMap;
 /**
  * @Author：huchenfei
  * @ClassName：FirstUniqChar
- * @Description：387. 字符串中的第一个唯一字符
+ * @Description： 387. 字符串中的第一个唯一字符
  * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
  * <p>
  * 案例:
- * <p>
  * s = "leetcode"
  * 返回 0.
  * <p>
  * s = "loveleetcode",
  * 返回 2.
- * <p>
- * <p>
  * 注意事项：您可以假定该字符串只包含小写字母。
  * @Date：2019/6/20 20:03
  * @Version： 1.0
@@ -24,10 +21,10 @@ import java.util.HashMap;
 public class FirstUniqChar {
     public static void main(String... args) {
         String s = "loveleetcode";
-        System.out.println(firstUniqChar1(s));
+        System.out.println(firstUniqChar(s));
     }
 
-    public static int firstUniqChar(String s) {
+    private static int firstUniqChar(String s) {
         for (int i = 0; i < s.length(); i++) {
             int count = 0;
             for (int j = 0; j < s.length(); j++) {
@@ -45,7 +42,7 @@ public class FirstUniqChar {
         return -1;
     }
 
-    public static int firstUniqChar1(String s) {
+    private static int firstUniqChar1(String s) {
         HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             String s1 = String.valueOf(s.charAt(i));
