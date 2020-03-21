db15a00ebe1c6b869fe012526485539f29ccd0c0
huchenfei <15732054931@163.com>
2020-03-21 14:44:47 +0800

    水壶问题

---
+++ src/solution/q_per_day/CanMeasureWater.java
@@ -0,0 +1,52 @@
+package solution.q_per_day;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author huchenfei
+ * @version 1.0
+ * @date 2020/3/21 12:18
+ * @description 365. 水壶问题
+ * 有两个容量分别为 x升 和 y升 的水壶以及无限多的水。
+ * 请判断能否通过使用这两个水壶，从而可以得到恰好 z升 的水？
+ * 如果可以，最后请用以上水壶中的一或两个来盛放取得的 z升 水。
+ * 你允许：
+ * 装满任意一个水壶
+ * 清空任意一个水壶
+ * 从一个水壶向另外一个水壶倒水，直到装满或者倒空
+ * <p>
+ * 示例 1: (From the famous "Die Hard" example)
+ * 输入: x = 3, y = 5, z = 4
+ * 输出: True
+ * <p>
+ * 示例 2:
+ * 输入: x = 2, y = 6, z = 5
+ * 输出: False
+ * @className CanMeasureWater
+ */
+public class CanMeasureWater {
+
+    public static void main(String[] args) {
+        System.out.println(canMeasureWater(3, 5, 4));
+    }
+
+    private static boolean canMeasureWater(int x, int y, int z) {
+        if(z > x + y){
+            return false;
+        }
+        Set<Integer> set = new HashSet<>();
+        return process(x, y, z, set);
+    }
+
+    private static boolean process(int x, int y, int z, Set<Integer> set){
+        if(set.contains(z)){
+            return false;
+        }
+        if(z == x || z == y || z == Math.abs(x - y) || z == (x + y)){
+            return true;
+        }
+        set.add(z);
+        return process(x, y, Math.abs(x - z), set) || process(x, y, Math.abs(y - z), set);
+    }
+}
